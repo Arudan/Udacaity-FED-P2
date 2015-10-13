9fd972904171349ca120e2933d581d505db71f64
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2015-10-13 08:47:38 +0200

    Added enimies and player

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
 // Enemies our player must avoid
-var Enemy = function(row, speed) {
+var Enemy = function(column, row, speed) {
     /*
     * Class function for the enemies, using the pseudoclassical model
-    * Takes two parameters, row and speed;
-    * Row is used to determin the position on the y axis, as enemies can only be
-    * in row 1 to 3.
+    * Takes three parameters, y, x and speed;
+    * y is an integer number between 1 and 3. It is used to determin the
+    * position on the y axis, as enemies can only be in row 1 to 3.
+    * x determins the position on the x axis; It's an integer number which gets
+    * multiplied by the number of pixel of the sprite width
     */
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = row * 83;
+    this.x = (column * 101);
+    this.y = (row * 70);
     this.speed = speed;
-
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  this.x += (speed * dt);
+  if (this.x > 606) {
+    this.x = -101;
+  }
+  this.render();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,19 +34,42 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function () {
     this.sprite = 'images/char-boy.png';
+    this.x = 3 * 101;
+    this.y = 5 * 83;
 };
-Player.prototype.update = function (dt) {};
+Player.prototype.update = function (dt) {
+  this.render();
+};
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-Player.prototype.handleInput = function (e) {
-
+Player.prototype.handleInput = function (key) {
+  if (key === 'up') {
+    this.y += 83;
+  } else if (key === 'left') {
+    this.x += -101;
+  } else if (key === 'right') {
+    this.x += 101;
+  } else if (key === 'down') {
+    this.y -= 83;
+  }
 };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemyGenerator = function (n) {
+  var enemyList = [];
+  for (var i = 0; i <= n; i++) {
+    row = Math.floor(Math.random() * 3) + 1;
+    column = Math.floor(Math.random() * 5) - 1;
+    speed = Math.floor(Math.random() * 30) + 20;
+    var enemy = new Enemy(column, row, speed);
+    enemyList.push(enemy);
+  }
+  return enemyList;
+};
 
-var allEnemies = [];
+var allEnemies = enemyGenerator(3);
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
