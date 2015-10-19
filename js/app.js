085b3ff52d2ec359656300a50b14b0b7a5670903
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2015-10-19 08:45:33 +0200

    feat: add dynamic enemy generator

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var gameInit = function(){
+var gameInit = function() {
   gameReset();
   mEngine.renderBackground();
   player.render();
@@ -8,18 +8,18 @@ var gameInit = function(){
   gameStart();
 };
 
-var gameStart = function(){
+var gameStart = function() {
   ctx.clearRect(0, 0, 505, 606);
   addPlayerListener();
   mEngine.init();
 };
 
-var gameStop = function(){
+var gameStop = function() {
   removePlayerListener();
   mEngine.stop();
 };
 
-var gameReset = function(){
+var gameReset = function() {
   mEngine.stop();
   this.map = 0;
   this.level = 1;
@@ -29,10 +29,10 @@ var gameReset = function(){
 
 var nextLevel = function() {
   level++;
-  if (level > 5) {
+  if (level > 10) {
     level = 1;
     map++;
-    if (map > 3){
+    if (map > 2) {
       console.log('victory');
     }
   }
@@ -41,34 +41,52 @@ var nextLevel = function() {
 };
 
 var enemyGenerator = function() {
-  var enemyList = [];
-  var enemyMatrix = [];
-  for (var i = 0; i < level; i++) {
-    row = Math.floor(Math.random() * 3) + 1;
-    column = Math.floor(Math.random() * 5) - 1;
-    speed = (100 * level) / (level);
-    var enemy = new Enemy(column, row, speed);
-    enemyList.push(enemy);
+  /** First, we build the enemyMatrix, rappresenting every possible position for
+   * the enemies in the map. We do so to be sure no enemies gets created on top
+   * of another
+   */
+  var enemiesMap = enemiesMaps[map];
+  var enemiesMatrix = [];
+  for (var row = 0; row < enemiesMap.length; row++) {
+    for (var col = 0; col < 5; col++) {
+      var matrixItem = [
+        (col + 1), enemiesMap[row][0], enemiesMap[row][1]
+      ];
+      enemiesMatrix.push(matrixItem);
+      console.log(matrixItem);
+    }
   }
-  return enemyList;
+  var enemiesList = [];
+  for (var i = 0; i < level; i++) {
+    speed = (100 * level) / (level);
+    var r = Math.floor(Math.random() * enemiesMatrix.length);
+    var enemyArray = enemiesMatrix[r];
+    enemiesMatrix.splice(r, 1);
+    console.log(r, enemyArray);
+    var enemy = new Enemy(
+      enemyArray[0], enemyArray[1], (enemyArray[2] * speed)
+    );
+    enemiesList.push(enemy);
+  }
+  return enemiesList;
 };
 
 var playerListener = function(e) {
   // This listens for key presses and sends the keys to your
   // Player.handleInput() method. You don't need to modify this.
-    var allowedKeys = {
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
-    };
-    player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 };
 
 var addPlayerListener = function() {
   /* This listens for key presses and sends the keys to
-  * Player.handleInput() method.
-  */
+   * Player.handleInput() method.
+   */
   document.addEventListener('keyup', playerListener);
 };
 
@@ -79,11 +97,11 @@ var removePlayerListener = function() {
 var menuListener = function(e) {
   // This listens for key presses and sends the keys to your
   // Player.handleInput() method. You don't need to modify this.
-    var allowedKeys = {
-      38: 'up',
-      40: 'down'
-    };
-    // player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+    38: 'up',
+    40: 'down'
+  };
+  // player.handleInput(allowedKeys[e.keyCode]);
 };
 
 var addMenuListener = function() {
--- js/levels.js
+++ js/levels.js
@@ -28,17 +28,25 @@ var maps = [
   ]
 ];
 
-var enemies = [
+/** This array is used to tell the game where to generate enemies on each map.
+* Each array inside the main array rappresents a level, so we shall call them
+* level-arrays. Inside each level-array are the row-arrays, which convey to the
+* generateEnemies function the position and characteristics of the enemies form
+* that map.
+* Inside the row array, the first number is the row index from the top, the
+* second number is the speed parameter of the enemies for that row.
+*/
+var enemiesMaps = [
   // map 1
   [
-    [1, 1.5], [2, 1], [3, 1]
+    [1, 1.5], [2, 1.25], [3, 1]
   ],
   // map 2
   [
-    [1, 1.5], [3, 1.5]
+    [1, 2], [3, 1.5], [4, 1.25]
   ],
   // map 3
   [
-    [2, 2], [4, 1.5]
+    [2, 2.5], [4, 2]
   ]
 ];
